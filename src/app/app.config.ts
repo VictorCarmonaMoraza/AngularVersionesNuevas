6ba646ef2a1ddea43b764abc3b967f3d008ff9d4
Victor Carmona <victorcarmona9035.com>
2025-08-25 06:24:18 +0200

    Nuevo provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,10 @@
 import { registerLocaleData } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
 import localeEs from '@angular/common/locales/es';
 import {
   ApplicationConfig,
-  importProvidersFrom,
   LOCALE_ID,
-  provideZoneChangeDetection,
+  provideZoneChangeDetection
 } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
@@ -18,13 +17,13 @@ registerLocaleData(localeEs, 'es');
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(HttpClientModule),
     { provide: LOCALE_ID, useValue: 'es' },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: SpinnerInterceptor,
       multi: true,
     },
+    provideHttpClient(withFetch()),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right',
