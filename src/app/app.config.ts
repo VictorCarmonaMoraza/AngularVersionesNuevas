6c1676c4d03710ceffb4a22b481af0919e940a16
Victor Carmona <victorcarmona9035.com>
2025-08-25 06:42:42 +0200

    Functional interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { registerLocaleData } from '@angular/common';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import localeEs from '@angular/common/locales/es';
 import {
   ApplicationConfig,
@@ -18,12 +18,7 @@ registerLocaleData(localeEs, 'es');
 export const appConfig: ApplicationConfig = {
   providers: [
     { provide: LOCALE_ID, useValue: 'es' },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: SpinnerInterceptor,
-      multi: true,
-    },
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([SpinnerInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: 'toast-bottom-right',
--- src/app/shared/interceptors/spinner.interceptor.ts
+++ src/app/shared/interceptors/spinner.interceptor.ts
@@ -6,22 +6,17 @@ import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
+  HttpInterceptorFn,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
-export class SpinnerInterceptor implements HttpInterceptor {
-  private readonly _spinnerService = inject(SpinnerService);
-
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    this._spinnerService.show();
-    return next
-      .handle(request)
-      .pipe(finalize(() => this._spinnerService.hide()));
-  }
-}
+export const SpinnerInterceptor: HttpInterceptorFn = (req, next) => {
+  const _spinnerService = inject(SpinnerService);
+  _spinnerService.show();
+  return next(req).pipe( finalize(() => {
+      // ⏳ esperamos 2 segundos antes de ocultar el spinner
+      setTimeout(() => _spinnerService.hide(), 2000);
+    }));
+};
